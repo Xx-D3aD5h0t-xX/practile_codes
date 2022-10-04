121a2e7e34f9eb029cc014905dc5d808d9a93a60
D3 <ihaveapc55@gmail.com>
2022-10-04 13:20:50 +0530

    testing

--- 11/8.py
+++ 11/8.py
@@ -1,8 +1,6 @@
-l = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+l = ['zero', 'one', 'two', 'three', 'four',
+     'five', 'six', 'seven', 'eight', 'nine']
+
 
 no = int(input("Enter the number: "))
-
 print(f"The corresponding word of {no} is: {l[no]}")
-
-
-
